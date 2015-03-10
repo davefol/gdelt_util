b9ac74e9d588c873e486b12b7d5c132120df9a36
Dave Fol <dof5@cornell.edu>
2015-03-10 02:23:37 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gdelt_util',
+      version='0.1',
+      description='Utilites exploring GDELT data set',
+      url='http://github.com/davefol/gdelt_util',
+      author='Dave Fol',
+      author_email='dof5@cornell.edu',
+      license='GNU',
+      packages=['gdelt_util'],
+      zip_safe=False)
